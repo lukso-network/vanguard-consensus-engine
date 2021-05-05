5aed6bb7f5c8813d3e5981fcf501d9272ea7d071
mxmar <macht4tech@gmail.com>
2021-05-05 17:21:25 +0200

    Feature: 9 vanguard subscriber - debug tests;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- beacon-chain/rpc/subscriber/api/api_backend_test.go
+++ beacon-chain/rpc/subscriber/api/api_backend_test.go
@@ -7,6 +7,8 @@ import (
 	types "github.com/prysmaticlabs/eth2-types"
 	ethpb "github.com/prysmaticlabs/ethereumapis/eth/v1alpha1"
 	mock "github.com/prysmaticlabs/prysm/beacon-chain/blockchain/testing"
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/feed"
+	statefeed "github.com/prysmaticlabs/prysm/beacon-chain/core/feed/state"
 	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
 	dbTest "github.com/prysmaticlabs/prysm/beacon-chain/db/testing"
 	"github.com/prysmaticlabs/prysm/beacon-chain/rpc/beacon"
@@ -31,6 +33,85 @@ func (api *OrchestratorMock) SyncNode() error {
 	return nil
 }
 
+func TestAPIBackend_SubscribeNewEpochEvent(t *testing.T) {
+	consensusChannel := make(chan interface{})
+	helpers.ClearCache()
+	db := dbTest.SetupDB(t)
+	ctx := context.Background()
+
+	config := params.BeaconConfig().Copy()
+	oldConfig := config.Copy()
+	config.SlotsPerEpoch = 32
+	params.OverrideBeaconConfig(config)
+
+	defer func() {
+		params.OverrideBeaconConfig(oldConfig)
+	}()
+	testStartTime := time.Now()
+
+	stateNotifier := new(mock.ChainService).StateNotifier()
+	state, err := testutil.NewBeaconState()
+	require.NoError(t, err)
+	require.NoError(t, state.SetSlot(0))
+
+	bs := &beacon.Server{
+		BeaconDB: db,
+		FinalizationFetcher: &mock.ChainService{
+			Genesis: testStartTime,
+			FinalizedCheckPoint: &ethpb.Checkpoint{
+				Epoch: 0,
+			},
+		},
+		GenesisTimeFetcher: &mock.ChainService{
+			Genesis: testStartTime,
+		},
+		StateGen: stategen.New(db),
+		StateNotifier: stateNotifier,
+		HeadFetcher: &mock.ChainService{
+			State: state,
+		},
+	}
+
+	minimalConsensusEvent := &feed.Event{
+		Type: statefeed.BlockProcessed,
+		Data: nil,
+	}
+
+	stateFeed := stateNotifier.StateFeed()
+
+	apiBackend := APIBackend{
+		BeaconChain:       *bs,
+	}
+
+	apiBackend.SubscribeNewEpochEvent(ctx, types.Epoch(0), consensusChannel)
+	shouldGather := 1
+	received := make([]*events.MinimalEpochConsensusInfo, 0)
+
+	var sendWaitGroup sync.WaitGroup
+	sendWaitGroup.Add(shouldGather)
+
+	go func() {
+		for {
+			consensusInfo := <-consensusChannel
+
+			if nil != consensusInfo {
+				received = append(received, consensusInfo.(*events.MinimalEpochConsensusInfo))
+			}
+		}
+	}()
+
+	sentTo := stateFeed.Send(minimalConsensusEvent)
+	sendWaitGroup.Wait()
+	assert.Equal(t, 1, sentTo)
+	assert.Equal(t, 0, len(received))
+
+	sendWaitGroup.Add(shouldGather)
+	require.NoError(t, state.SetSlot(params.BeaconConfig().SlotsPerEpoch))
+	sentTo = stateFeed.Send(minimalConsensusEvent)
+	sendWaitGroup.Wait()
+	assert.Equal(t, 1, sentTo)
+	assert.Equal(t, shouldGather, len(received))
+}
 
 func TestAPIBackend_GetMinimalConsensusInfo(t *testing.T) {
 	consensusChannel := make(chan interface{})
