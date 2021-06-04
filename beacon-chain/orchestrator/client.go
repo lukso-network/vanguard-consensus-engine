9c2057ec19da305283fbf2cbe238de6262343036
mxmar <macht4tech@gmail.com>
2021-06-04 15:51:39 +0200

    Feature: 21 orchestrator rpc client for blocks confirmation flow - modified client to be aware of nil in loop;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- beacon-chain/orchestrator/client.go
+++ beacon-chain/orchestrator/client.go
@@ -78,12 +78,14 @@ func (orc *RPCClient) ConfirmVanBlockHashes(ctx context.Context, blockHashes []*
 
 	// It's for now until we unify orc-van communication flow
 	for _, orcBlockStatus := range orcBlockStatuses {
-		blockStatus := &vanTypes.ConfirmationResData{
-			Slot:   types.Slot(orcBlockStatus.Slot),
-			Hash:   orcBlockStatus.Hash,
-			Status: vanTypes.Status(orcBlockStatus.Status),
+		if orcBlockStatus != nil {
+			blockStatus := &vanTypes.ConfirmationResData{
+				Slot:   types.Slot(orcBlockStatus.Slot),
+				Hash:   orcBlockStatus.Hash,
+				Status: vanTypes.Status(orcBlockStatus.Status),
+			}
+			blockStatuses = append(blockStatuses, blockStatus)
 		}
-		blockStatuses = append(blockStatuses, blockStatus)
 	}
 
 	return blockStatuses, nil
