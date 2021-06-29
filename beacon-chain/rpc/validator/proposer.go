aa2e20dec18cca94cccf3225b184e57e78fe0e5a
Atif Anowar <atif@lukso.io>
2021-06-29 17:35:03 +0600

    initialize chain service into pending queue fetcher

--- beacon-chain/rpc/validator/proposer.go
+++ beacon-chain/rpc/validator/proposer.go
@@ -63,6 +63,7 @@ func (vs *Server) GetBlock(ctx context.Context, req *ethpb.BlockRequest) (*ethpb
 	// pending queue is not empty that means syncing and verification process does not complete yet so skipped the
 	// slot
 	if vs.EnableVanguardNode {
+		log.WithField("slot", req.Slot).Debug("checking pending queue length before preparing block")
 		if err := vs.PendingQueueFetcher.CanPropose(); err != nil {
 			return nil, status.Errorf(codes.Unavailable, "Pending queue is not fully processed yet")
 		}
