80252d39e0af59326c3ea9d708709feb1b3ee92f
Atif Anowar <radi.cse072@gmail.com>
2021-10-08 17:57:41 +0200

    Debugging memory issue

--- beacon-chain/blockchain/head.go
+++ beacon-chain/blockchain/head.go
@@ -365,13 +365,13 @@ func (s *Service) notifyNewHeadEvent(
 	})
 
 	// If vanguard node is enabled then trigger epoch info publisher rpc srevice
-	if s.enableVanguardNode {
-		proposerIndices, pubKeys, err := helpers.ProposerIndicesInCache(newHeadState)
-		if err != nil {
-			return errors.Wrap(err, "could not get proposer indices for publishing")
-		}
-		s.triggerEpochInfoPublisher(newHeadSlot, proposerIndices, pubKeys)
-	}
+	//if s.enableVanguardNode {
+	//	proposerIndices, pubKeys, err := helpers.ProposerIndicesInCache(newHeadState)
+	//	if err != nil {
+	//		return errors.Wrap(err, "could not get proposer indices for publishing")
+	//	}
+	//	s.triggerEpochInfoPublisher(newHeadSlot, proposerIndices, pubKeys)
+	//}
 	return nil
 }
 
